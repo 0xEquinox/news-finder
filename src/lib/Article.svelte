298c209ea41e3634cff7118b94a77b55a453dfde
will <williamJmcdonald@protonmail.com>
2023-06-07 22:54:55 -0700

    Added a feature

--- src/lib/Article.svelte
+++ src/lib/Article.svelte
@@ -6,10 +6,9 @@
 
 <div class="rounded-lg mb-2 m-4 mx-8 flex bg-slate-300">
 	<div>
-		<h1 class="font-semibold bg-red-500 rounded-tl-lg p-2 text-white">{ data["title"] }</h1>
+		<a href={data["url"]} class="block font-semibold bg-red-500 rounded-tl-lg p-2 text-white">{ data["title"] }</a>
 		<h1 class="p-2">{ data["description"]}</h1>
 
-	
 		<a class="px-2 pt-1 font-semibold" href={data["source"]["url"]}>Soruce: {data["source"]["name"]}</a>
 	</div>
 	<img class="w-24 rounded-r-lg" src={data["image"]} alt="" />
